2dfcb377ed515ca2e018cc0908c3a501af72d965
Bronson Zgeb <bronson.zgeb@gmail.com>
2014-01-29 17:08:06 -0500

    Maybe now

--- public/javascripts/static_pages.js
+++ public/javascripts/static_pages.js
@@ -1,13 +1,13 @@
 (function() {
-  $("#mail-form").bind("ajaxComplete", function(xhr, status) {
+  $("#mail-form").bind("ajax:complete", function(xhr, status) {
     return $("#response").html(status.responseText);
-  }).bind("beforeSend", function() {
+  }).bind("ajax:beforeSend", function() {
     return $("#response").html("<p>Sending...</p>");
   });
 
-  $("#mail-form-phone").bind("ajaxComplete", function(xhr, status) {
+  $("#mail-form-phone").bind("ajax:complete", function(xhr, status) {
     return $("#response-phone").html(status.responseText);
-  }).bind("beforeSend", function() {
+  }).bind("ajax:beforeSend", function() {
     return $("#response-phone").html("<p>Sending...</p>");
   });
 }).call(this);
